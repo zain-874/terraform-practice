cb6266798a40793d608c933ad860085a09b07c72
Zain Ul Abedin <zaneulabadien@gmail.com>
2023-07-19 23:04:43 +0500

    add autoscaling groups, data sources, launch config

--- main.tf
+++ main.tf
@@ -2,23 +2,49 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "web" {
-    ami             = "ami-0fb653ca2d3203ac1"
-    instance_type   = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+data "aws_vpc" "default" {
+  default = true
+}
 
-user_data = <<-EOF
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "web" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-user_data_replace_on_change = true
-
-    tags            = {
-        "Name"      = "Terraform example"
-    } 
+# Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.web.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+ 
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -37,7 +63,3 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  description = "showing public ip"
-  value       = aws_instance.web.public_ip
-}
